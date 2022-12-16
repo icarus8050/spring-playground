e2e42976194c54c2f16a7ea0da054924d8ac0267
icarus8050 <icarus8050@naver.com>
2022-12-16 20:19:42 +0900

    project plugin setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("org.springframework.boot") version "2.7.6"
+    id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.21"
+    kotlin("plugin.spring") version "1.7.21"
     application
 }
 
@@ -14,6 +17,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
 tasks.test {
@@ -21,7 +27,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "13"
 }
 
 application {
